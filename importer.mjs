e91bd2e427ebef3262d243c1e4a2754515f7dea4
Chris Powell <cmpowell97@gmail.com>
2023-11-10 03:16:02 -0500

    done regular data entry

--- importer.mjs
+++ importer.mjs
@@ -54,7 +54,6 @@ const map = new Map([
             }
     ]}],
     [197, {
-        exclude: '',
         entries: [
             {
                 name: 'Archmage',
@@ -316,7 +315,7 @@ const map = new Map([
             {
                 name: 'Dragon, Sea',
                 features: ['Steam Breath', 'Water Spout']
-            }
+            },
             {
                 name: 'Dragon, Swamp',
                 features: ['Smog Breath']
@@ -590,14 +589,611 @@ const map = new Map([
                 features: ['Bound', 'Invisible', 'Tracking']
             }
     ]}],
-    
-    
-    [666, {
-        exclude: '',
+    [228, {
         entries: [
             {
-                name: 'APE, SNOW',
-                features: ['Thick Fur']
+                name: 'Jellyfish',
+                features: ['Toxin']
+            },
+            {
+                name: 'Knight',
+                features: ['Oath']
+            },
+            {
+                name: 'Kobold',
+                features: ['Dodge']
+            },
+            {
+                name: 'Kobold, Sorcerer',
+                features: ['Dodge', 'Scorpion Sting', 'Spider Swarm']
+            }
+    ]}],
+    [229, {
+        entries: [
+            {
+                name: 'Kraken',
+                features: ['Impervious', 'Crush', 'Lightning Bolt', 'Storm']
+            },
+            {
+                name: 'Leech, Giant',
+                features: ['Attach']
+            }
+    ]}],
+    [230, {
+        entries: [
+            {
+                name: 'Leprechaun',
+                features: ['Alert', 'Slippery', 'Fool\'s Gold', 'Illusion', 'Invisibility']
+            },
+            {
+                name: 'Lich',
+                features: ['Supreme Undead', 'Phylactery', 'Paralysis', 'Flight', 'Null', 'Shadow Leap', 'Sigil of Doom', 'Wither']
+            }
+    ]}],
+    [231, {
+        entries: [
+            {
+                name: 'Lion'
+            },
+            {
+                name: 'Lizardfolk'
+            },
+            {
+                name: 'Mage',
+                features: ['Arcane Armor', 'Blast', 'Cancel', 'Levitate', 'Snare']
+            },
+            {
+                name: 'Mammoth',
+                features: ['Thick Fur', 'Charge']
+            }
+    ]}],
+    [232, {
+        entries: [
+            {
+                name: 'Manta Ray, Giant',
+                features: ['Poison']
+            },
+            {
+                name: 'Manticore',
+                features: ['Spikes']
+            },
+            {
+                name: 'Mastiff'
+            },
+            {
+                name: 'Medusa',
+                features: ['Godborn', 'Petrify', 'Poison']
+            },
+            {
+                name: 'Mimic',
+                features: ['Stick']
+            }
+    ]}],
+    [233, {
+        entries: [
+            {
+                name: 'Minotaur',
+                features: ['Charge']
+            },
+            {
+                name: 'Moose'
+            }
+    ]}],
+    // [234, {************************************************************
+    //     entries: [
+    //         {
+    //             name: 'Minotaur',
+    //             features: ['Charge']
+    //         },
+    //         {
+    //             name: 'Moose'
+    //         }
+    // ]}],
+    [236, {
+        entries: [
+            {
+                name: 'Mummy',
+                features: ['Supreme Undead', 'Desiccated', 'Necrosis']
+            },
+            {
+                name: 'Mushroomfolk',
+                features: ['Sunblind', 'Telepathic']
+            }
+    ]}],
+    [237, {
+        entries: [
+            {
+                name: 'Naga',
+                features: ['Poison', 'Agony', 'Hypnotize', 'Whispers']
+            },
+            {
+                name: 'Naga, Bone',
+                features: ['Greater Undead']
+            },
+            {
+                name: 'Nightmare',
+                features: ['Impervious']
+            }
+    ]}],
+    // [237, {********************************************************************
+    //     entries: [
+    //         {
+    //             name: 'Naga',
+    //             features: ['Poison', 'Agony', 'Hypnotize', 'Whispers']
+    //         },
+    //         {
+    //             name: 'Naga, Bone',
+    //             features: ['Greater Undead']
+    //         },
+    //         {
+    //             name: 'Nightmare',
+    //             features: ['Impervious']
+    //         }
+    // ]}],
+    [239, {
+        entries: [
+            {
+                name: 'Ochre Jelly',
+                features: ['Split']
+            },
+            {
+                name: 'Octopus, Giant',
+                features: ['Grab', 'Ink']
+            },
+            {
+                name: 'Ogre'
+            },
+            {
+                name: 'Oni',
+                features: ['Shapeshift', 'Fade', 'Hellfrost', 'Mist']
+            }
+    ]}],
+    [240, {
+        entries: [
+            {
+                name: 'Orc',
+                features: ['Rage']
+            },
+            {
+                name: 'Orc, Chieftan',
+                features: ['Rage']
+            },
+            {
+                name: 'Otyugh',
+                features: ['Disease']
+            }
+    ]}],
+    [241, {
+        entries: [
+            {
+                name: 'Primordial Slime',
+                features: ['Impervious', 'Dissolve']
+            },
+            {
+                name: 'Rime Walker',
+                features: ['Impervious', 'Ice Aura']
+            },
+            {
+                name: 'Void Spawn',
+                features: ['Impervious', 'Toxin']
+            },
+            {
+                name: 'Void Spider',
+                features: ['Impervious', 'Phase', 'Poison']
+            }
+    ]}],
+    [242, {
+        entries: [
+            {
+                name: 'Owlbear',
+                features: ['Crush']
+            },
+            {
+                name: 'Panther'
+            },
+            {
+                name: 'Peasant'
+            },
+            {
+                name: 'Pegasus'
+            }
+    ]}],
+    [243, {
+        entries: [
+            {
+                name: 'Phoenix',
+                features: ['Impervious', 'Explosion', 'Heat Aura', 'Rebirth']
+            },
+            {
+                name: 'Piranha, Swarm',
+                features: ['Savage']
+            },
+            {
+                name: 'Pirate'
+            },
+            {
+                name: 'Priest',
+                features: ['Anoint', 'Healing Touch', 'Holy Flame', 'Rebuke']
+            }
+    ]}],
+    [244, {
+        entries: [
+            {
+                name: 'Purple Worm',
+                features: ['Poison', 'Swallow']
+            },
+            {
+                name: 'Rakshasa',
+                features: ['Impervious', 'Mesmerism', 'Weakness']
+            }
+    ]}],
+    [245, {
+        entries: [
+            {
+                name: 'Rat',
+                features: ['Disease']
+            },
+            {
+                name: 'Rat, Giant',
+                features: ['Disease']
+            },
+            {
+                name: 'Rat, Dire',
+                features: ['Disease']
+            },
+            {
+                name: 'Rat, Swarm',
+                features: ['Disease']
+            }
+    ]}],
+    // [246, {
+    //     entries: [
+    //         {
+    //             name: 'Rat',
+    //             features: ['Disease']
+    //         },
+    //         {
+    //             name: 'Rat, Giant',
+    //             features: ['Disease']
+    //         },
+    //         {
+    //             name: 'Rat, Dire',
+    //             features: ['Disease']
+    //         },
+    //         {
+    //             name: 'Rat, Swarm',
+    //             features: ['Disease']
+    //         }
+    // ]}],
+    [247, {
+        entries: [
+            {
+                name: 'Reaver',
+                features: ['Bloodlust']
+            },
+            {
+                name: 'Remorhaz',
+                features: ['Impervious', 'Melt', 'Swallow']
+            }
+    ]}],
+    [248, {
+        entries: [
+            {
+                name: 'Rhinoceros',
+                features: ['Charge']
+            },
+            {
+                name: 'Roc',
+                features: ['Grab']
+            },
+            {
+                name: 'Roper',
+                features: ['Impervious', 'Grab', 'Pull', 'Tendrils']
+            }
+    ]}],
+    [249, {
+        entries: [
+            {
+                name: 'Rot Flower',
+                features: ['Toxin']
+            },
+            {
+                name: 'Rust Monster',
+                features: ['Corrosive']
+            },
+            {
+                name: 'Sahuagin',
+                features: ['Half-Amphibious']
+            },
+            {
+                name: 'Salamander',
+                features: ['Impervious', 'Heat Aura']
+            },
+            {
+                name: 'Scarab, Swarm'
+            },
+            {
+                name: 'Scarecrow',
+                features: ['Scream']
+            }
+    ]}],
+    [250, {
+        entries: [
+            {
+                name: 'Scorpion',
+                features: ['Poison']
+            },
+            {
+                name: 'Scorpion, Giant',
+                features: ['Grab', 'Poison']
+            },
+            {
+                name: 'Shadow',
+                features: ['Drain']
+            }
+    ]}],
+    [251, {
+        entries: [
+            {
+                name: 'Shambling Mound',
+                features: ['Impervious', 'Engulf']
+            },
+            {
+                name: 'Shark'
+            },
+            {
+                name: 'Shark, Megalodon',
+                features: ['Fearless']
+            },
+            {
+                name: 'Siren',
+                features: ['Song']
+            },
+            {
+                name: 'Skeleton',
+                features: ['Undead']
+            },
+            {
+                name: 'Smilodon'
+            }
+    ]}],
+    [252, {
+        entries: [
+            {
+                name: 'Snake, Cobra',
+                features: ['Poison']
+            },
+            {
+                name: 'Snake, Giant',
+                features: ['Constrict']
+            },
+            {
+                name: 'Snake, Swarm',
+                features: ['Poison']
+            },
+            {
+                name: 'Soldier'
+            }
+    ]}],
+    [253, {
+        entries: [
+            {
+                name: 'Sphinx',
+                features: ['Roar', 'Gate', 'Omens', 'Riddle', 'Time Bend', 'Unmake']
+            }
+    ]}],
+    [254, {
+        entries: [
+            {
+                name: 'Spider',
+                features: ['Poison']
+            },
+            {
+                name: 'Spider, Giant',
+                features: ['Poison']
+            },
+            {
+                name: 'Spider, Swarm',
+                features: ['Poison']
+            },
+            {
+                name: 'Stingbat',
+                features: ['Blood Drain']
+            },
+            {
+                name: 'Strangler',
+                features: ['Stealthy', 'Strangle']
+            }
+    ]}],
+    // [255, {**************************
+    //     entries: [
+    //         {
+    //             name: 'Spider',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Spider, Giant',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Spider, Swarm',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Stingbat',
+    //             features: ['Blood Drain']
+    //         },
+    //         {
+    //             name: 'Strangler',
+    //             features: ['Stealthy', 'Strangle']
+    //         }
+    // ]}],
+        // [256, {**************************
+    //     entries: [
+    //         {
+    //             name: 'Spider',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Spider, Giant',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Spider, Swarm',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Stingbat',
+    //             features: ['Blood Drain']
+    //         },
+    //         {
+    //             name: 'Strangler',
+    //             features: ['Stealthy', 'Strangle']
+    //         }
+    // ]}],
+        // [258, {**************************
+    //     entries: [
+    //         {
+    //             name: 'Spider',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Spider, Giant',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Spider, Swarm',
+    //             features: ['Poison']
+    //         },
+    //         {
+    //             name: 'Stingbat',
+    //             features: ['Blood Drain']
+    //         },
+    //         {
+    //             name: 'Strangler',
+    //             features: ['Stealthy', 'Strangle']
+    //         }
+    // ]}],
+    [259, {
+        entries: [
+            {
+                name: 'Thief',
+                features: ['Stealthy', 'Backstab']
+            },
+            {
+                name: 'Thug'
+            },
+            {
+                name: 'Treant',
+                features: ['Animate Tree']
+            },
+            {
+                name: 'Troll',
+                features: ['Regenerate']
+            }
+    ]}],
+    [260, {
+        entries: [
+            {
+                name: 'Troll, Frost',
+                features: ['Impervious', 'Regenerate']
+            },
+            {
+                name: 'Unicorn',
+                features: ['Healing Horn']
+            },
+            {
+                name: 'Vampire',
+                features: ['Supreme Undead', 'Blood Drain', 'Charm', 'Shapechange', 'Vampire']
+            }
+    ]}],
+    [261, {
+        entries: [
+            {
+                name: 'Vampire Spawn',
+                features: ['Greater Undead', 'Blood Drain', 'Vampire']
+            },
+            {
+                name: 'Violet Fungus'
+            },
+            {
+                name: 'Vampire',
+                features: ['Supreme Undead', 'Blood Drain', 'Charm', 'Shapechange', 'Vampire']
+            }
+    ]}],
+    [262, {
+        exclude: '262.*warriors.',
+        entries: [
+            {
+                name: 'Viperian'
+            },
+            {
+                name: 'Viperian, Ophid',
+                features: ['Impervious']
+            },
+            {
+                name: 'Viperian, Wizard',
+                features: ['Hiss', 'Summon Cobra', 'Venom', 'Whispers']
+            }
+    ]}],
+    [263, {
+        entries: [
+            {
+                name: 'Vulture',
+                features: ['Carrion Tracker']
+            },
+            {
+                name: 'Wasp, Giant',
+                features: ['Venom']
+            },
+            {
+                name: 'Wererat',
+                features: ['Impervious', 'Lycanthropy']
+            },
+            {
+                name: 'Werewolf',
+                features: ['Impervious', 'Lycanthropy']
+            },
+            {
+                name: 'Wight',
+                features: ['Greater Undead', 'Life Drain']
+            }
+    ]}],
+    [264, {
+        entries: [
+            {
+                name: 'Will-o\'-Wisp',
+                features: ['Life Drain']
+            },
+            {
+                name: 'Wolf',
+                features: ['Pack Hunter']
+            },
+            {
+                name: 'Wolf, Dire',
+                features: ['Pack Hunter']
+            },
+            {
+                name: 'Wolf, Winter',
+                features: ['Impervious', 'Frost Breath']
+            }
+    ]}],
+    [265, {
+        entries: [
+            {
+                name: 'Worg'
+            },
+            {
+                name: 'Wraith',
+                features: ['Greater Undead', 'Incorporeal', 'Life Drain']
+            },
+            {
+                name: 'Wyvern',
+                features: ['Poison']
+            },
+            {
+                name: 'Zombie',
+                features: ['Undead', 'Relentless']
             }
     ]}],
 ])
